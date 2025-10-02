dd13f232a763f41ff08f20ace11c88429a345e7e
ABHIRAM J <abhiramj132006@gmail.com>
2025-10-02 23:41:02 +0530

    Update doubleendedqueue.c

--- prgms/doubleendedqueue.c
+++ prgms/doubleendedqueue.c
@@ -63,15 +63,29 @@ int deleterear() {
     if (front == rear) {
         front = rear = -1;
     } else {
-        rear = (rear - 1 + size) % size;  // 
+        rear = (rear - 1 + size) % size;  
     }
     return item;
+}
+void displayqueue() {
+    if (isempty()) {
+        printf("Queue is empty\n");
+        return;
+    }
+    int i = front;
+    printf("Queue elements: ");
+    while (i != -1) {
+        printf("%d ", q[i]);
+        if (i == rear) break;
+        i = (i + 1) % size;
+    }
+    printf("\n");
 }
 
 int main() {
     int choice, item;
     do {
-        printf("\n1: Insert at rear\n2: Delete from front\n3: Insert at front\n4: Delete from rear\n5: Exit\n");
+        printf("\n1: Insert at rear\n2: Delete from front\n3: Insert at front\n4: Delete from rear\n5: displayqueue\n6: Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
         switch (choice) {
@@ -96,12 +110,14 @@ int main() {
                     printf("Deleted item is %d\n", item);
                 break;
             case 5:
+                displayqueue();
+                break;
+            case 6:
                 printf("Exiting...\n");
                 break;
             default:
                 printf("Invalid choice, please try again.\n");
         }
-    } while (choice != 5);
+    } while (choice != 6);
     return 0;
 }
-
